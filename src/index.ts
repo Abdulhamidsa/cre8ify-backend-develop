9f0b7b9162341dd2d3a6b16cad2af143b5403a0c
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2025-07-09 15:36:04 +0200

    fix: add SQL client connection to server startup process

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { connectMongoDB } from './common/config/mongo.connection.js';
 import { SECRETS } from './common/config/secrets.js';
+import { getSQLClient } from './common/config/sql.client.js';
 import Logger from './common/utils/logger.js';
 import app from './server.js';
 
@@ -8,6 +9,7 @@ const PORT = SECRETS.port;
 const start = async (): Promise<void> => {
   try {
     await connectMongoDB();
+    await getSQLClient();
     Logger.info('Connected to MongoDB');
     app.listen(PORT, () => {
       Logger.info(`Server running at http://localhost:${PORT}`);
