baa0bf6b36ece2b7c561b0b1269366d1668aa69f
Abdulhamidsa <alsaati.abdulhamid@gmail.com>
2026-02-09 17:16:46 +0100

    feat: add utility functions to set and clear cookies with custom names

--- src/common/utils/cookie.utils.ts
+++ src/common/utils/cookie.utils.ts
@@ -1,4 +1,4 @@
-import { CookieOptions } from 'express';
+import { CookieOptions, Response } from 'express';
 
 import { SECRETS } from '../config/secrets.js';
 
@@ -12,4 +12,18 @@ export const getCookieOptions = (tokenType: 'access' | 'refresh'): CookieOptions
   cookieOptions.maxAge = tokenType === 'access' ? SECRETS.accessTokenMaxAge : SECRETS.refreshTokenMaxAge;
 
   return cookieOptions;
+};
+
+// New utility functions for managing cookies with custom names
+export const setCookie = (res: Response, name: string, value: string, options: CookieOptions): void => {
+  res.cookie(name, value, options);
+};
+
+export const clearCookie = (res: Response, name: string): void => {
+  res.cookie(name, '', {
+    expires: new Date(0),
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  });
 };
